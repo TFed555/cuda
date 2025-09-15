4a3ba7fc3b96ae88444a2e70f9bc1f9518bbe363
TFed555 <thedgeofwave@gmail.com>
2025-09-15 17:12:47 +0000

    fixed pointers

--- work1/benchmarks/benchmark_lab1.cu
+++ work1/benchmarks/benchmark_lab1.cu
@@ -10,12 +10,6 @@ static void BENCHMARK_addVec_cpu(benchmark::State &state)
     float *a, *b, *c;
     init_vectors(&a, &b, &c, N);
 
-    for (int i = 0; i <= N; i++)
-        {
-            a[i] = i;
-            b[i] = i;
-        }
-
     for (auto _ : state)
     {
         addVec_cpu(a, b, c, N);
@@ -63,5 +57,61 @@ static void BENCHMARK_addVec(benchmark::State &state)
 }
 
 BENCHMARK(BENCHMARK_addVec);
+
+//замер с выделением памяти CPU
+static void BENCHMARK_addVec_cpu2(benchmark::State &state)
+{
+    const int N = 256;
+
+    for (auto _ : state)
+    {
+        float *a, *b, *c;
+        init_vectors(&a, &b, &c, N);
+        addVec_cpu(a, b, c, N); 
+        free_vectors(a, b, c);
+    }
+
+}
+
+BENCHMARK(BENCHMARK_addVec_cpu2);
+
+//замер с выделением памяти GPU
+static void BENCHMARK_addVec2(benchmark::State &state)
+{
+  const int N = 256;
+  size_t size = N * sizeof(float);
+
+    for (auto _ : state)
+    {
+        float *host_a, *host_b, *host_c;
+        init_vectors(&host_a, &host_b, &host_c, N);
+
+        float* device_a;
+        float* device_b;
+        float* device_c;
+
+        copy_vectors(host_a, host_b, host_c, &device_a, &device_b, &device_c, N);
+
+        int threadsPerBlock = 256;
+        int blocksPerGrid =(N + threadsPerBlock - 1) / threadsPerBlock;
+
+        addVec<<<blocksPerGrid, threadsPerBlock>>>(device_a, device_b, device_c, N);
+
+        cudaMemcpy(host_c, device_c, size, cudaMemcpyDeviceToHost);
+
+        cudafree_vectors(device_a, device_b, device_c);
+        free_vectors(host_a, host_b, host_c);
+    }
+
+    // cudaFree(device_a);
+    // cudaFree(device_b);
+    // cudaFree(device_c);
+
+    // free(host_a);
+    // free(host_b);
+    // free(host_c);
+}
+
+BENCHMARK(BENCHMARK_addVec2);
 
 BENCHMARK_MAIN();
