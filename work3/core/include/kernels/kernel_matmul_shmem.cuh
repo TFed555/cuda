74b948c493a3ac1dc19304b6fa08362bc57364fc
TFed555 <thedgeofwave@gmail.com>
2025-10-13 16:08:27 +0000

    modified kernel_matmul_shmem

--- work3/core/include/kernels/kernel_matmul_shmem.cuh
+++ work3/core/include/kernels/kernel_matmul_shmem.cuh
@@ -17,11 +17,17 @@ __global__ void kernel_matmul_shmem(MatrixView<AtomT> a, MatrixView<AtomT> b,
 
   __shared__ AtomT a_sub_sh[BLOCK_SIZE][BLOCK_SIZE];
   __shared__ AtomT b_sub_sh[BLOCK_SIZE][BLOCK_SIZE];
-  int ch = (a.ncols() + BLOCK_SIZE - 1) / BLOCK_SIZE;
-  for (int k = 0; k < ch; k++) {
-    //if (i < res.nrows() && j < res.ncols())
-    a_sub_sh[row][col] = a(BLOCK_SIZE*blockRow + row, BLOCK_SIZE*k + col);
-    b_sub_sh[row][col] = b(BLOCK_SIZE*k + row, BLOCK_SIZE*blockCol + col);
+
+  int sub_rowA = BLOCK_SIZE*blockRow + row;
+  int sub_colB = BLOCK_SIZE*blockCol + col;
+
+  for (int k = 0; k < (a.ncols() + BLOCK_SIZE - 1) / BLOCK_SIZE; k++) {
+    int sub_colA = BLOCK_SIZE*k + col;
+    int sub_rowB = BLOCK_SIZE*k + row;
+    a_sub_sh[row][col] = (sub_rowA < a.nrows() && sub_colA < a.ncols()) ? 
+                            a(sub_rowA, sub_colA) : 0;
+    b_sub_sh[row][col] = (sub_rowB < b.nrows() && sub_colB < b.ncols()) ?
+                            b(sub_rowB, sub_colB) : 0;
     __syncthreads();
 
     for (int i = 0; i < BLOCK_SIZE; i++) {
@@ -29,8 +35,9 @@ __global__ void kernel_matmul_shmem(MatrixView<AtomT> a, MatrixView<AtomT> b,
     }
     __syncthreads();
   }
-
-  res(BLOCK_SIZE*blockRow + row, BLOCK_SIZE*blockCol + col) = sum;
+  
+  if (sub_rowA < res.nrows() && sub_colB < res.ncols())
+    res(sub_rowA, sub_colB) = sum;
 }
 
 
--- work3/core/src/lab3.cu
+++ work3/core/src/lab3.cu
@@ -2,46 +2,55 @@
 #include "../include/matrix_operators.cuh"
 #include <vector>
 
+
+//пример
 int main() {
   using atom_t = float;
+  int M = 10;
+  int N = 8;
+  int K = 5;  
+  int hostN_A = M*K;
+  int hostN_B = K*N;
+  int hostN_C = M*N;
 
-  Matrix<atom_t> A(4,4);
-  Matrix<atom_t> B(4,4);
-  Matrix<atom_t> C(4,4);
+  Matrix<atom_t> A(M,K);
+  Matrix<atom_t> B(K,N);
+  Matrix<atom_t> C(M,N);
   
   A.fill(1.0f);
   B.fill(10.0f);
 
   C = A.view() * B.view();
-  std::vector<atom_t> hostA(64);
+  std::vector<atom_t> hostA(hostN_A);
   A.data().copy_to_host(hostA.data());
-  std::vector<atom_t> hostB(64);
+  std::vector<atom_t> hostB(hostN_B);
   B.data().copy_to_host(hostB.data());
-  std::vector<atom_t> hostC(64);
+  std::vector<atom_t> hostC(hostN_C);
   C.data().copy_to_host(hostC.data());
 
     std::cout << "Matrix A from GPU:\n";
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            std::cout << hostA[i * 4 + j] << " ";
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < K; ++j) {
+            std::cout << hostA[i * K + j] << " ";
         }
         std::cout << "\n";
   }
 
     std::cout << "Matrix B from GPU:\n";
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            std::cout << hostB[i * 4 + j] << " ";
+    for (int i = 0; i < K; ++i) {
+        for (int j = 0; j < N; ++j) {
+            std::cout << hostB[i * K + j] << " ";
         }
         std::cout << "\n";
   }
 
   std::cout << "Matrix C from GPU:\n";
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            std::cout << hostC[i * 4 + j] << " ";
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
+            std::cout << hostC[i * K + j] << " ";
         }
         std::cout << "\n";
   }
+
   return 0;
 }
