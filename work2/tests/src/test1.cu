42079ae633b663ba7515056859e14c4bd6a9eb6d
striction10 <ilia_shcherbakov1234@mail.ru>
2025-10-13 15:59:32 +0000

    upd tests

--- work2/tests/src/test1.cu
+++ work2/tests/src/test1.cu
@@ -33,3 +33,71 @@ TYPED_TEST(MatrixTest, SumMatrix) {
     }
   }
 }
+
+TYPED_TEST(MatrixTest, SubMatrix) {
+  Matrix<float> a(2, 2);
+  Matrix<float> b(2, 2);
+  Matrix<float> res(2, 2);
+  MatrixXf expected = MatrixXf::Constant(2, 2, 0.0f);
+
+  float val = 1.0f;
+
+  a.init(val);
+  b.init(val);
+
+  std::vector<float> hostRes(4);
+
+  res = a.view() - b.view();
+  res.data().copy_to_host(hostRes.data());
+
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      EXPECT_EQ(hostRes[i*2+j], expected(i, j));
+    }
+  }
+}
+
+TYPED_TEST(MatrixTest, MultiMatrix) {
+  Matrix<float> a(2, 2);
+  Matrix<float> b(2, 2);
+  Matrix<float> res(2, 2);
+  MatrixXf expected = MatrixXf::Constant(2, 2, 8.0f);
+
+  float val = 2.0f;
+
+  a.init(val);
+  b.init(val);
+
+  std::vector<float> hostRes(4);
+
+  res = a.view() * b.view();
+  res.data().copy_to_host(hostRes.data());
+
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      EXPECT_EQ(hostRes[i*2+j], expected(i, j));
+    }
+  }
+}
+
+TYPED_TEST(MatrixTest, MultiScalarMatrix) {
+  Matrix<float> a(2, 2);
+
+  float val = 3.0f;
+  a.init(val);
+  
+  float scalar = 2.0f;
+
+  MatrixXf expected = MatrixXf::Constant(2, 2, 6.0f);
+  
+  Matrix<float> res = a.view() * scalar;
+  
+  std::vector<float> hostRes(4);
+  res.data().copy_to_host(hostRes.data());
+  
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      EXPECT_FLOAT_EQ(hostRes[i*2+j], expected(i, j));
+    }
+  }
+}
