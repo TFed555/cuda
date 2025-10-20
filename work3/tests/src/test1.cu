cb44b62abda6248b5621296737635219046eb2c9
striction10 <ilia_shcherbakov1234@mail.ru>
2025-10-20 14:44:57 +0000

    upd tests

--- work3/tests/src/test1.cu
+++ work3/tests/src/test1.cu
@@ -61,3 +61,39 @@ TYPED_TEST(MatrixTest, MultiMatrixSmallSize) {
     }
   }
 }
+
+TYPED_TEST(MatrixTest, MultiMatrixBigSize) {
+  MatmulNaiveStrategy<float> naive;
+  MatmulShmemStrategy<float> shmem;
+  std::vector<MatmulStrategy<float>*> strategies = {&naive, &shmem};
+
+  const std::vector<float> sizes = {127, 128, 129, 512};
+  const float val = 2.0f;
+  for (auto* strategy : strategies) {
+    for (size_t i = 0; i < sizes.size(); ++i) {
+      int m = sizes[i];
+      for (size_t j = 0; j < sizes.size(); ++j) {
+        int k = sizes[j];
+        for (size_t l = 0; l < sizes.size(); ++l) {
+          int n = sizes[l];
+
+          Matrix<float> a(m, k);
+          Matrix<float> b(k, n);
+          a.fill(val);
+          b.fill(val);
+
+          a.set_strategy(strategy);
+
+          Matrix<float> res = a * b;
+
+          MatrixXf eigen_a = MatrixXf::Constant(m, k, val);
+          MatrixXf eigen_b = MatrixXf::Constant(k, n, val);
+          MatrixXf expected = eigen_a * eigen_b;
+          MatrixXf test_val = toEigenMatrix(res, m, n);
+
+          EXPECT_TRUE(test_val.isApprox(expected, 1e-5f));
+        }
+      }
+    }
+  }
+}
